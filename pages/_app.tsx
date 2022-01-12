46f8df7c2c180524d2faeaff2351902ecc5857a6
Aman Thakur <builtbyaman@gmail.com>
2022-01-12 23:45:11 +0530

    Added Chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,41 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "reset-css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: "#f5f5f5",
+      200: "#eeeeee",
+      300: "#E0E0E0",
+      400: "#BDBDBD",
+      500: "#9E9E9E",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
+    },
+  },
+  components: {
+    Button: {
+      varients: {
+        link: {
+          ":focus": {
+            outline: "none",
+            boxShadow: "none",
+          },
+        },
+      },
+    },
+  },
+});
 
-export default MyApp
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
+
+export default MyApp;
