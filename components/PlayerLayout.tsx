5ad147ca954361ac5d90e22fd7b7d321e9d1e705
Aman Thakur <builtbyaman@gmail.com>
2022-01-18 14:02:23 +0530

    Added sidebar

--- components/PlayerLayout.tsx
+++ components/PlayerLayout.tsx
@@ -1,7 +1,19 @@
 import { Box } from "@chakra-ui/react";
 
 const PlayerLayout = ({ children }) => {
-  return <Box>Layout {children}</Box>;
+  return (
+    <Box width="100vw" h="100vh">
+      <Box top={0} w="250px" left="0" position="absolute">
+        sidebar
+      </Box>
+      <Box ml="250px" mb="100px">
+        {children}
+      </Box>
+      <Box pos="absolute" left="0" bottom="0">
+        player
+      </Box>
+    </Box>
+  );
 };
 
 export default PlayerLayout;
