f1a8260e0c49ba3f3c7580482c260b2f941ab9ef
Aman Thakur <builtbyaman@gmail.com>
2022-02-01 16:01:17 +0530

    Added signin

---
+++ pages/api/signin.ts
@@ -0,0 +1,40 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import cookie from "cookie";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "../../lib/prisma";
+
+export default async function signin(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { email, password } = req.body;
+  const user = await prisma.user.findUnique({
+    where: { email },
+  });
+  if (user && bcrypt.compareSync(password, user.password)) {
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        time: Date.now(),
+      },
+      "hello",
+      { expiresIn: "8h" }
+    );
+
+    res.setHeader(
+      "Set-Cookie",
+      cookie.serialize("TRAX_ACCESS_TOKEN", token, {
+        httpOnly: true,
+        maxAge: 8 * 60 * 60,
+        path: "/",
+        sameSite: "lax",
+        secure: process.env.NODE_ENV === "production",
+      })
+    );
+    return res.json(user);
+  }
+  res.status(401);
+  res.json("Email or password is wrong");
+}
