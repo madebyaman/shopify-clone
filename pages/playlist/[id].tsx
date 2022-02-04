6863f09629b786437035bd53486dea682a8a4f9e
Aman Thakur <builtbyaman@gmail.com>
2022-02-05 00:41:36 +0530

    fixed authentication server side

--- pages/playlist/[id].tsx
+++ pages/playlist/[id].tsx
@@ -39,9 +39,19 @@ const Playlist = ({ playlist }) => {
 export const validateToken = (token) => jwt.verify(token, "hello");
 
 export const getServerSideProps = async ({ query, req }) => {
-  const { id } = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
+  let user;
+  try {
+    user = validateToken(req.cookies.TRAX_ACCESS_TOKEN);
+  } catch (e) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/signin",
+      },
+    };
+  }
   const playlist = await prisma.playlist.findFirst({
-    where: { id: Number(query.id), userId: id },
+    where: { id: Number(query.id), userId: user.id },
     include: {
       songs: {
         include: {
